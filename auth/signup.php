796868e17c49b6f5266dafc2c343b69f21c8dcc8
Alaa Mahrous <alamhrws8@gmail.com>
2026-07-20 17:52:38 +0300

    error fixing

--- auth/signup.php
+++ auth/signup.php
@@ -6,7 +6,7 @@ $username = filterRequest("username");
 $password = sha1("password");
 $email = filterRequest("email");
 $phone = filterRequest("phone");
-$verfiycode     = "0";
+$verfiycode     = rand(10000 , 99999);
 
 $stmt = $con->prepare("SELECT * FROM users WHERE user_email = ? OR user_phone = ? ");
 $stmt->execute(array($email, $phone));
@@ -20,8 +20,9 @@ if ($count > 0) {
         "user_password" =>  $password,
         "user_email" => $email,
         "user_phone" => $phone,
-        "user_verfiycode" => "0",
+        "user_verfiycode" => $verfiycode ,
     );
+    //sendEmail($email , "Verfiy Code Ecommerce" , "Verfiy Code $verfiycode") ; 
     insertData("users" , $data) ; 
 
 }
--- auth/verifycode.php
+++ auth/verifycode.php
@@ -7,7 +7,7 @@ $email  = filterRequest("email") ;
 $verfiy = filterRequest("verifycode") ; 
 
 
-$stmt = $con->prepare("SELECT * FROM users WHERE users_email = '$email' AND users_verfiycode = '$verfiy' ") ; 
+$stmt = $con->prepare("SELECT * FROM users WHERE user_email = '$email' AND user_verfiycode = '$verfiy' ") ; 
  
 $stmt->execute() ; 
 
@@ -15,9 +15,9 @@ $count = $stmt->rowCount() ;
 
 if ($count > 0) {
  
-    $data = array("users_approve" => "1") ; 
+    $data = array("user_approve" => "1") ; 
 
-    updateData("users" , $data , "users_email = '$email'");
+    updateData("users" , $data , "user_email = '$email'");
 
 }else {
  printFailure("verifycode not Correct") ; 
